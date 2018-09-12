7d88349b5f55160ee14a9bd5aaa18b0a480947de
Abdulla thanseeh <thanseehabdulla@gmail.com>
2018-09-12 18:21:14 +0530

    view front end added

--- thanseehsite/polls/urls.py
+++ thanseehsite/polls/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('home', views.home, name='home'),
+    path('test/<pk>/', views.TestView.as_view(), name="test")
+
 ]
--- thanseehsite/polls/views.py
+++ thanseehsite/polls/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
-
+from django.views.generic import TemplateView, DetailView
+from .models import Question
 
 def index(request):
     return HttpResponse("Hello, thanseeh . You're at the polls index.")
@@ -21,3 +22,13 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
+
+class TestView(DetailView):
+    model = Question
+    template_name = 'index.html'
+    context_object_name = 'question'
