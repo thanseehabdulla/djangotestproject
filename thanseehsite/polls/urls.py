7ad4540957e8869e74a77c37eb77bafd97771ac6
Abdulla thanseeh <thanseehabdulla@gmail.com>
2018-09-13 12:25:22 +0530

    new front view display added

--- thanseehsite/polls/urls.py
+++ thanseehsite/polls/urls.py
@@ -5,6 +5,9 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('home', views.home, name='home'),
-    path('test/<pk>/', views.TestView.as_view(), name="test")
+    path('page', views.page, name='page'),
+    path('display', views.display, name='page'),
+    path('vote/<int:question_id>', views.vote, name='votess'),
+    path('test/<pk>/', views.TestView.as_view(), name="test"),
 
 ]
--- thanseehsite/polls/views.py
+++ thanseehsite/polls/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
+from django.template import loader
 from django.views.generic import TemplateView, DetailView
 from .models import Question
+
 
 def index(request):
     return HttpResponse("Hello, thanseeh . You're at the polls index.")
@@ -24,11 +26,25 @@ def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
 
 
-def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
-
-
 class TestView(DetailView):
     model = Question
     template_name = 'index.html'
     context_object_name = 'question'
+
+
+def page(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # list home comprehenssion
+    # r = b if a > b else b
+    # if a is True:
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+
+def display(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('replay.html')
+    context = {
+            'latest_question_list': latest_question_list,
+             }
+    return HttpResponse(template.render(context, request))
